e3ea72caf2489bfe45f8ec69ed5beef327aabfcc
aone <aone.jga@gmail.com>
2024-11-29 16:38:39 +0100

    CLI does not work as expected if app not running
    BetterDisplay CLI commands hang a while and file if the application is not running. Using DistributedNotificationCenter instead.

--- MultiSoundChanger/Sources/Utils/BetterDisplay.swift
+++ MultiSoundChanger/Sources/Utils/BetterDisplay.swift
@@ -6,36 +6,38 @@
 //  Copyright © 2021 Dmitry Medyuho. All rights reserved.
 //
 
-import Foundation
+import AppKit
 
 enum BetterDisplay {
     private static let executablePath = "/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay"
+    private static let requestNotificationName = NSNotification.Name("com.betterdisplay.BetterDisplay.request")
     
-    private static func isInstalled() -> Bool {
-        FileManager.default.fileExists(atPath: BetterDisplay.executablePath)
+    private struct IntegrationNotificationRequestData: Codable {
+        var uuid: String?
+        var commands: [String] = []
+        var parameters: [String: String?] = [:]
     }
+
+    private static let isInstalled: Bool = FileManager.default.fileExists(atPath: executablePath)
     
-    ///
-    /// While there's DistributedNotificationCenter based integration available by using CLIinstead
-    /// we assure the command will execute even if BetterDisplay wasn't already running.
-    ///
-    @discardableResult
-    static private func set(_ parameter: String, value: String, deviceName: String) -> String? {
-        if BetterDisplay.isInstalled() {
-            let bdCmd = "\(BetterDisplay.executablePath) set -n=\"\(deviceName)\" -\(parameter)=\(value)"
-            Logger.debug("Executing BetterDisplay command: \(bdCmd)")
-            return Runner.shell(bdCmd)
+    private static func set(_ parameter: String, value: String, deviceName: String) {
+        if !isInstalled {
+            return
         }
-        return nil
+        let data = IntegrationNotificationRequestData(uuid: UUID().uuidString, commands: ["n", "set"], parameters: [deviceName: "", parameter: value])
+        do {
+            let encodedData = try JSONEncoder().encode(data)
+            if let encodedDataString = String(data: encodedData, encoding: .utf8) {
+                DistributedNotificationCenter.default().postNotificationName(requestNotificationName, object: encodedDataString, userInfo: nil, deliverImmediately: true)
+            }
+        } catch {}
     }
     
-    @discardableResult
-    static func setVolume(_ volume: Float, deviceName: String) -> String? {
-        return BetterDisplay.set("volume", value: String(volume), deviceName: deviceName)
+    static func setVolume(_ volume: Float, deviceName: String) {
+        set("volume", value: String(volume), deviceName: deviceName)
     }
     
-    @discardableResult
-    static func mute(_ mute: Bool, deviceName: String) -> String? {
-        return BetterDisplay.set("mute", value: mute ? "on" : "off", deviceName: deviceName)
+    static func mute(_ mute: Bool, deviceName: String) {
+        set("mute", value: mute ? "on" : "off", deviceName: deviceName)
     }
 }
